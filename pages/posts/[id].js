89b99d4e538998caa6a6bdc600087f9cae203521
西脇　佳宏 <nytennis0713555555@gmail.com>
2021-04-25 16:41:29 +0900

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+// ③「②」で取得したファイルを展開する。
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+// ①ここで静的なものを作成する可能性のものを指定しておく。
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// ②実際に選択されたもののファイル情報を取得してくる。
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
